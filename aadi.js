2ba94c72656f0ee7dfead9664003a2bfcb323ec0
Adarsh sinha <Adarshsinha123>
2022-12-10 10:06:41 -0800

    Aadii

---
+++ aadi.js
@@ -0,0 +1,16 @@
+let number=5;
+
+let count=0;
+for(let j=1;j<=number;i++){
+
+if(number%j==0){
+   count++
+}
+}
+
+if(count==1){
+console.log("prime number");
+
+else{
+  console.log("not prime number);
+}
